c56de41fbef0b241f482e4d4273732fcc6cfb5e8
Steve Taranto <staranto@gmail.com>
2025-11-14 06:07:11 -0500

    refactor: add trace log level.

--- internal/log/log.go
+++ internal/log/log.go
@@ -12,15 +12,35 @@ import (
 	"github.com/apex/log"
 )
 
+var traceEnabled bool
+
 // InitLogger sets up Apex with a custom handler and a log level from the
 // TFCTL_LOG env variable.
 func InitLogger() {
-	level := strings.ToUpper(os.Getenv("TFCTL_LOG"))
-	if level == "" {
-		level = "ERROR"
+	envLevel := strings.ToLower(os.Getenv("TFCTL_LOG"))
+	if envLevel == "" {
+		envLevel = "error"
+	}
+	traceEnabled = envLevel == "trace"
+	var apexLevel log.Level
+	switch envLevel {
+	case "trace":
+		apexLevel = log.DebugLevel // Show debug and above for trace
+	case "debug":
+		apexLevel = log.DebugLevel
+	case "info":
+		apexLevel = log.InfoLevel
+	case "warn":
+		apexLevel = log.WarnLevel
+	case "error":
+		apexLevel = log.ErrorLevel
+	case "fatal":
+		apexLevel = log.FatalLevel
+	default:
+		apexLevel = log.ErrorLevel
 	}
 	log.SetHandler(&CustomHandler{})
-	log.SetLevelFromString(level)
+	log.SetLevel(apexLevel)
 }
 
 // CustomHandler formats log messages and writes to stdout
@@ -29,8 +49,62 @@ type CustomHandler struct{}
 // HandleLog implements the log.Handler interface
 func (h *CustomHandler) HandleLog(e *log.Entry) error {
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	level := strings.ToUpper(e.Level.String())
 	message := e.Message
-	fmt.Fprintf(os.Stdout, "%s %.1s %s\n", timestamp, level, message)
+	level := "?"
+	if strings.HasPrefix(message, "TRACE: ") {
+		level = "T"
+		message = message[7:]
+	} else {
+		switch e.Level {
+		case log.DebugLevel:
+			level = "D"
+		case log.InfoLevel:
+			level = "I"
+		case log.WarnLevel:
+			level = "W"
+		case log.ErrorLevel:
+			level = "E"
+		case log.FatalLevel:
+			level = "F"
+		}
+	}
+	fmt.Fprintf(os.Stdout, "%s %s %s\n", timestamp, level, message)
 	return nil
+}
+
+// Tracef logs at Trace level (below Debug).
+func Tracef(format string, args ...interface{}) {
+	if traceEnabled {
+		log.Debug("TRACE: " + fmt.Sprintf(format, args...))
+	}
+}
+
+// Debugf logs at Debug level.
+func Debugf(format string, args ...interface{}) {
+	log.Debugf(format, args...)
+}
+
+// Infof logs at Info level.
+func Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+
+// Errorf logs at Error level.
+func Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+
+// Debug logs at Debug level.
+func Debug(msg string) {
+	log.Debug(msg)
+}
+
+// Warnf logs at Warn level.
+func Warnf(format string, args ...interface{}) {
+	log.Warn(fmt.Sprintf(format, args...))
+}
+
+// WithError returns an entry with error.
+func WithError(err error) *log.Entry {
+	return log.WithError(err)
 }
